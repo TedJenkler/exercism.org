8699afa9c3238cb18bcd92b476339b7f7aa7235b
Ted Jenkler <tedjenkler@gmail.com>
2024-09-14 21:43:30 +0200

    added 28

---
+++ 28-Resistor Color.js
@@ -0,0 +1,37 @@
+export const colorCode = (color) => {
+    console.log(color)
+    switch(color) {
+        case 'black':
+          return 0
+        break;
+        case 'brown':
+          return 1
+        break;
+        case 'red':
+          return 2
+        break;
+        case 'orange':
+          return 3
+        break;
+        case 'yellow':
+          return 4
+        break;
+        case 'green':
+          return 5
+        break;
+        case 'blue':
+          return 6
+        break;
+        case 'violet':
+          return 7
+        break;
+        case 'grey':
+          return 8
+        break;
+        case 'white':
+          return 9
+        break;
+    }
+  };
+  
+  export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
