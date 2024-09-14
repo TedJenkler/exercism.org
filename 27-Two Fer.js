62d3d1684f259bd8f4409d7927dadf5de26f41bb
Ted Jenkler <tedjenkler@gmail.com>
2024-09-14 21:37:48 +0200

    added 27

---
+++ 27-Two Fer.js
@@ -0,0 +1,7 @@
+export const twoFer = (name) => {
+    if(name !== null && name !== undefined) {
+      return `One for ${name}, one for me.`
+    }else {
+      return 'One for you, one for me.'
+    }
+  };
