d02955c94cde7f19796d1e171d6bf87675a6411c
Ted Jenkler <tedjenkler@gmail.com>
2024-11-02 21:26:36 +0100

    added 03 py

---
+++ exercism python/03-Ghost Gobble Arcade Game.py
@@ -0,0 +1,15 @@
+"""Functions for implementing the rules of the classic arcade game Pac-Man."""
+
+
+def eat_ghost(power_pellet_active, touching_ghost):
+    return power_pellet_active and touching_ghost
+
+def score(touching_power_pellet, touching_dot):
+    return touching_power_pellet or touching_dot
+
+
+def lose(power_pellet_active, touching_ghost):
+    return not power_pellet_active and touching_ghost
+
+def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
+    return has_eaten_all_dots and power_pellet_active or not touching_ghost
