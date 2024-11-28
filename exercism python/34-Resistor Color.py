ea93fab70ab388ee504471352a1c2010d38c3a65
Ted Jenkler <tedjenkler@gmail.com>
2024-11-29 00:24:04 +0100

    added 34 py

---
+++ exercism python/34-Resistor Color.py
@@ -0,0 +1,18 @@
+def color_code(color):
+    map_of_colors = { "black": 0, "brown": 1, "red": 2, "orange": 3, "yellow": 4, "green": 5, "blue": 6, "violet": 7, "grey": 8, "white": 9}
+    return map_of_colors[color]
+
+
+def colors():
+    return [
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "grey",
+    "white",
+]
